5167fd5fa4290a8c2678eb1d6bbb855cbe7da685
Erika <eyardumian@gmail.com>
2017-12-12 21:47:56 -0800

    Update Details.js

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,32 +1,15 @@
 import React from "react";
 import "./Details.css";
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
-const Details = props =>
-  <form className="search">
-    <div className="form-group">
-      <label htmlFor="breed">Breed Name:</label>
-      <input
-        value={props.search}
-        onChange={props.handleInputChange}
-        name="breed"
-        list="breeds"
-        type="text"
-        className="form-control"
-        placeholder="Type in a dog breed to begin"
-        id="breed"
-      />
-      <datalist id="breeds">
-        {props.breeds.map(breed => <option value={breed} key={breed} />)}
-      </datalist>
-      <button
-        type="submit"
-        onClick={props.handleFormSubmit}
-        className="btn btn-success"
-      >
-        Search
-      </button>
-    </div>
-  </form>;
+class Details extends Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+)}
+
+
 
 export default Details;
